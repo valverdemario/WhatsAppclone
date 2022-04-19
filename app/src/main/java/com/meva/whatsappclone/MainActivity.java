528b3b5230ed4dc08f1c052134179b1b6ad7593b
MarioValverde <dev13@erillamhc.com>
2022-04-19 17:16:45 -0500

    se añadio la vista del codigo de verificacion

--- app/src/main/java/com/meva/whatsappclone/MainActivity.java
+++ app/src/main/java/com/meva/whatsappclone/MainActivity.java
@@ -2,13 +2,27 @@ package com.meva.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    MaterialButton mbLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mbLogin = findViewById(R.id.mBtnLogin);
+        mbLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,CodeVerificationActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
